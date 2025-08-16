71fad66abd5190264687c9bafe4364409fe3abe8
anurag458 <anuragappzily@gmail.com>
2025-08-16 15:49:58 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Cine Radar 🚀</h1>
+      <p>Welcome to my first React app!</p>
+    </div>
+  );
+}
+
+export default App;
